379248284a73724b411503c5020f2fac2570aaa8
kishininfosec <packman_19@proton.me>
2026-01-31 21:36:41 -0600

    local push

---
+++ Workshops/main.tf
@@ -0,0 +1,46 @@
+resource {
+  name  =
+  keep_locally  =
+}
+resource {
+  name  =
+}
+resource {
+  name  =
+}
+
+resource {
+  image  =
+  name  =
+  restart  =
+  shm_size =
+
+  ports {
+    internal
+    external
+  }
+  ports {
+    internal
+    external
+  }
+  volumes {
+    host_path
+    container_path
+    read_only
+  }
+  volumes {
+    host_path
+    container_path
+    read_only
+  }
+  volumes {
+    host_path
+    container_path
+    read_only
+  }
+  volumes {
+    host_path
+    container_path
+    read_only
+  },
+}
